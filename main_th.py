d306dbd4119e76bcc2f08cf4086210b908133dc8
saurabh-2905 <saurabhband2905@gmail.com>
2023-10-13 13:41:23 +0200

    working multi-thread and todo for base code

--- main_th.py
+++ main_th.py
@@ -1,64 +1,76 @@
-# TODO Add machine.reset() on error with hardware, set timer for restart if connection to TTN impossible 
-
-# Import Pysense libraries
+### Import Pysense libraries
 from pycoproc_2 import Pycoproc
 import pycom
-import time
 import _thread
-import json
+from network import LoRa
+import socket
+import time
 
 from LIS2HH12 import LIS2HH12
 from SI7006A20 import SI7006A20
-# Import of a file containing functions for connecting to TTN.
+### Import of a file containing functions for connecting to TTN.
 import LoRaConnection
 
-from lib.varlogger import VarLogger as logger
+from varlogger import VarLogger as logger
 
 ### initialize logger
+### initialize it outside the scope of any function to allow access to all the code
 logt = logger()
 
-# This function resides in the LoRaConnection.py file. It is used to create the connection to The Things Network and block further execution until a connection has been established
-LoRaConnection.connectToTTN()
+### put all the functionality in different functions to be able to run in multiple threads, use a class with methods as '@classmethod' so that we can pass class itslef as an argument and dont need to make an instance to be able to use class variables
+def main():
+    lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868)
+    ### init pyproc for reading data
+    py = Pycoproc()
+
+    ### Turn off the PyCom "Heartbeat" - the constant blinking of the indicator LED
+    pycom.heartbeat(False)
+
+    ### Set indicator LED to blue in order to signify that a successful connection has been established and that the sensor readout is now starting
+    pycom.rgbled(0x00008B) # blue
+    time.sleep(5)
+
+    ### Below is the initialisation of all hardware components that are connected to the LoPy4
+    si = SI7006A20(py)
+
+    ### The adx and itg variables hold instances of classes that represent the ADXL345 accelerometer and ITG3200 gyroscope respectively.
+    li = LIS2HH12(py)
+
+    ### The tenMinutes and thirtyMinutes variables hold values of ten and thirty minutes in seconds, respectively.
+    tenMinutes = 1 * 60
+    thirtyMinutes = 3 * tenMinutes
+
+    ### The variables below are used to store the last second from initialisation of the system in which data was transmitted to TTN or at which the temperature and humidity were captured and written to currentIntervalTemperatureHumidity.
+    lastTransmitTime = 0
+    lastTempHumCaptureTime = 0
+
+    ### This variable holds the current temperature and humidity until transmission. Temperature and humidity are written to this array every 10 minutes, transmission is every 30 minutes due to the limitations of TTN.
+    currentIntervalTemperature = []
+    currentIntervalHumidity = []
+
+    prevXAcceleration = None
+    prevYAcceleration = None
+    prevZAcceleration = None
+
+    ### This is the main part of the programme which runs in an infinite loop until the device is stopped or an error occurs. Indicator set to green to show arrival at main loop
+    pycom.rgbled(0x008B00) # green
+    time.sleep(5)
+
+    ### sense the data
+    acceleration = sense(li)
+
+    ### send the data via lora communication
+    # acquire lock
+    # write data to shared resource
+    # set the flag of communication 
+    # release the lock
+
+    ### Turn on the PyCom "Heartbeat" - the constant blinking of the indicator LED
+    pycom.heartbeat(True)
 
 
-# init pyproc for reading data
-py = Pycoproc()
 
-# Turn off the PyCom "Heartbeat" - the constant blinking of the indicator LED
-pycom.heartbeat(False)
-
-# Set indicator LED to red in order to signify that a successful connection has been established and that the sensor readout is now starting
-pycom.rgbled(0x8B0000) # red
-time.sleep(5)
-
-# # Below is the initialisation of all hardware components that are connected to the LoPy4
-si = SI7006A20(py)
-
-# # The adx and itg variables hold instances of classes that represent the ADXL345 accelerometer and ITG3200 gyroscope respectively.
-li = LIS2HH12(py)
-
-# The tenMinutes and thirtyMinutes variables hold values of ten and thirty minutes in seconds, respectively.
-tenMinutes = 1 * 60
-thirtyMinutes = 3 * tenMinutes
-
-# The variables below are used to store the last second from initialisation of the system in which data was transmitted to TTN or at which the temperature and humidity were captured and written to currentIntervalTemperatureHumidity.
-lastTransmitTime = 0
-lastTempHumCaptureTime = 0
-
-# This variable holds the current temperature and humidity until transmission. Temperature and humidity are written to this array every 10 minutes, transmission is every 30 minutes due to the limitations of TTN.
-currentIntervalTemperature = []
-currentIntervalHumidity = []
-
-prevXAcceleration = None
-prevYAcceleration = None
-prevZAcceleration = None
-
-# This is the main part of the programme which runs in an infinite loop until the device is stopped or an error occurs. Indicator set to green to show arrival at main loop
-pycom.rgbled(0x001000) # green
-while True:
-    # The current time variable gets updated constantly to hold the current time for this loop
-    currentTime = time.time()
-
+def sense(li):
     # The acceleration info is requested from the sensor at every loop
     acceleration = li.acceleration()
     print("Acceleration: " + str(acceleration))
@@ -66,68 +78,42 @@ while True:
     yAcceleration = acceleration[1]
     zAcceleration = acceleration[0]
 
-    # gyroResult = itg.result
-    # xGyro = gyroResult[0]
-    # yGyro = gyroResult[1]
-    # zGyro = gyroResult[2]                                     
+    return acceleration
+    
 
-    if (prevXAcceleration and prevYAcceleration and prevZAcceleration):
-        xDiff = abs(xAcceleration - prevXAcceleration)
-        yDiff = abs(yAcceleration - prevYAcceleration)
-        zDiff = abs(zAcceleration - prevZAcceleration)
+def process():
+    pass
 
-        xCondition = xDiff > 1.4
-        yCondition = yDiff > 0.51
-        zCondition = zDiff > 0.48
+def loracom():
+    
+    # check for the communication flag
+    # acquire the lock
+    # get the data from shared resource
+    # release the lock
 
-        shouldTriggerAlarm = (xCondition and yCondition) or (xCondition and zCondition) or (yCondition and zCondition)
+    ### create a LoRa socket
+    s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 
-        if (shouldTriggerAlarm):
-            print('Alarm triggered')
-            # buzzer.playAlarm()
-            # time.sleep(1)
-            # buzzer.stopAlarm()
-            LoRaConnection.sendSocketData(bytearray([1]))
+    ### make the socket blocking
+    ###(waits for the data to be sent and for the 2 receive windows to expire)
+    s.setblocking(False)
 
-    prevXAcceleration = xAcceleration
-    prevYAcceleration = yAcceleration
-    prevZAcceleration = zAcceleration
-        
-    # If it has been at least 10 minutes since the last capture of temperature and humidity, we get the data from the DHT11 and store it in lists
-    if (currentTime - lastTempHumCaptureTime >= tenMinutes or lastTempHumCaptureTime == 0):
-        lastTempHumCaptureTime = time.time()
-        print('Trying to capture temperature')
-        temperature = round(si.temperature())
-        humidity = round(si.humidity())
+    ### send some data
+    s.send(data)
 
-        currentIntervalTemperature.append(temperature)
-        currentIntervalHumidity.append(humidity)
-
-    # If it has been at least thirty minutes since the last transmission, we calculate the average of the captured temperature and humidity data and send it to the TTN using LoRaWAN sockets.
-    if(currentTime - lastTransmitTime >= thirtyMinutes):
-        lastTransmitTime = time.time()
-        print('Transmitting')
-        print(currentIntervalTemperature)
-        if (len(currentIntervalTemperature) > 0 and len(currentIntervalHumidity) > 0):
-            averageTemperature = int(sum(currentIntervalTemperature)/len(currentIntervalTemperature))
-            averageHumidity = int(sum(currentIntervalHumidity)/len(currentIntervalHumidity))
-            print('Clearing lists')
-            currentIntervalTemperature.clear()
-            currentIntervalHumidity.clear()
-            sensorData = {
-                "time": time.time(),
-                "temperature": averageTemperature,
-                "humidity": averageHumidity,
-                "xAcceleration": xAcceleration,
-                "yAcceleration": yAcceleration,
-                "zAcceleration": zAcceleration,
-            }
-            dataForSocket = bytes([averageTemperature, averageHumidity])
-            print(dataForSocket)
-            LoRaConnection.sendSocketData(dataForSocket)
+    ### make the socket non-blocking
+    ### (because if there's no data received it will block forever...)
+    s.close()
 
 
-    time.sleep_ms(1000)
+class threadcom:
+    @classmethod
+    def timerflag(cls):
+        pass
+
+    @classmethod
+    def sharedres(cls):
+        pass
 
 
-
+    main_thread = _thread.start_new_thread(main, ())
