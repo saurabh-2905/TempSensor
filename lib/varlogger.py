a608f5c00deae0f41390fd75f03fb781f577523b
saurabh-2905 <saurabhband2905@gmail.com>
2023-10-15 18:11:07 +0200

    thread_status, write_data, docs and classmethod

--- lib/varlogger.py
+++ lib/varlogger.py
@@ -3,61 +3,59 @@ import json
 import os
 
 class VarLogger:
-    def __init__(self):
-        self.data = []   ### store variale sequence
-        self.created_timestamp = utime.ticks_ms()  ### start time
-        self.data_dict = {}  ### store timestamp for each variable
-        self._catchpop = 0  ### temporary storage
-        self._write_count = 0  ### count to track writing frequency
-        self.write_name, self.trace_name = self.check_files()  ### avoid overwriting previous log
+    '''
+    This class is the part of the tool, it logs data and collects all the information required to monitor the system
+    all the code lines that are part of the tools are commented using '#/////'
+    '''
+    
+    data = []   ### store variale sequence
+    created_timestamp = utime.ticks_ms()  ### start time
+    data_dict = {}  ### store timestamp for each variable
+    _catchpop = 0  ### temporary storage
+    _write_count = 0  ### count to track writing frequency
 
+    ####### thread tracking
+    threads_info = dict() ### init a dictionary to store the status of each thread
 
-
-    def log(self, var='0', fun='0', clas='0', th='0'):
+    @classmethod
+    def log(cls, var='0', fun='0', clas='0', th='0'):
         '''
         var -> str = name of the variable
         '''
-        dict_keys = self.data_dict.keys()
+        dict_keys = cls.data_dict.keys()
 
         if var in dict_keys:
-            _varlist = self.data_dict[var]
+            _varlist = cls.data_dict[var]
 
             ### save only 500 latest values for each variable
             while len(_varlist) >= 500: 
-                self._catchpop = _varlist.pop(0)
+                cls._catchpop = _varlist.pop(0)
             
             _varlist += [utime.ticks_ms()]
-            self.data_dict[var] = _varlist
+            cls.data_dict[var] = _varlist
 
         else:
-            self.data_dict[var] = [utime.ticks_ms()]
+            cls.data_dict[var] = [utime.ticks_ms()]
 
         ### make the event name based on the scope
-        event = f'{th}_{clas}_{fun}_{var}'
+        event = '{}_{}_{}_{}'.format(th, clas, fun, var)
         ### log the sequence to trace file
-        self.log_seq(event)
+        cls.log_seq(event)
         
-        self._write_count +=1
-        print(self._write_count)
-
+        cls._write_count +=1
+        print(cls._write_count)
         ### write to flash approx every 6 secs (counting to 1000 = 12 ms)
-        if self._write_count >= 10:
-            self._write_count = 0
-            with open(self.write_name, 'w') as fp:
-                json.dump(self.data_dict, fp)
-                print('dict saved', self.write_name)
-
-            with open(self.trace_name, 'w') as fp:
-                json.dump(self.data, fp)
-                print('trace saved', self.trace_name)
+        if cls._write_count >= 10:
+            cls._write_count = 0
+            cls.write_data() ### save the data to flash
                 
 
-    
-    def log_seq(self, event):
-        self.data += [event]
+    @classmethod
+    def log_seq(cls, event):
+        cls.data += [event]
 
-
-    def check_files(self):
+    @classmethod
+    def check_files(cls):
         '''
         check for previous log and update the name to avoid overwriting
         '''
@@ -73,3 +71,33 @@ class VarLogger:
                 break
 
         return (_filename,_seqname)
+    
+    @classmethod
+    def write_data(cls):
+        write_name, trace_name = cls.check_files()  ### get the name to write the files and avoid overwriting previous log
+        with open(write_name, 'w') as fp:
+            json.dump(cls.data_dict, fp)
+            print('dict saved', write_name)
+
+        with open(trace_name, 'w') as fp:
+            json.dump(cls.data, fp)
+            print('trace saved', trace_name)
+
+    @classmethod
+    def thread_status(cls, thread_id=None, status=None):
+        '''
+        update or retrive the status of the thread. If no value is given to 'status' and 'thread_id' it will return the status of all the threads and it's ids
+        status = ['dead' or 'alive']
+        to update: pass arguments to thread_id and status
+        to get status: dont pass andy arguments
+        '''
+        ### update status of the thread if it is active or not
+        ids = cls.threads_info.keys()
+        
+        assert(status=='dead' or status=='active' or status==None)
+
+        ### if status is given then update the status for respective thread
+        if status!=None and thread_id!=None :
+            cls.threads_info[thread_id] = status
+        else:
+            return(ids, cls.threads_info)
