c779960a7db80f118d879004dcfd96915a29a044
saurabh-2905 <saurabhband2905@gmail.com>
2023-10-26 10:35:52 +0200

    test

--- trial.py
+++ trial.py
@@ -1,7 +1,9 @@
 import sys
 import utime
 from lib.varlogger import VarLogger as vl
+from machine import Timer
 
+###### catch traceback #####
 def trial():
     a = [1,2,3]
 
@@ -11,4 +13,23 @@ def trial():
     except Exception as e:
         vl.traceback(e)
         
-trial()
+#trial()
+
+##### test timer interrupt ######
+
+class Clock():
+    def start(self, time):
+        '''
+        time in ms
+        '''
+        self.alarm = Timer.Alarm(handler=self.cb, ms=time, periodic=True)
+
+    def cb(self, alarm):
+        #lock.acquire()
+        #control.clock_set()
+        #lock.release()
+        print('clock done')
+
+    def stop(self):
+        self.alarm.cancel()
+        print('clock stopped')
